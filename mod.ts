26928caebac0f54e9b3c64c4542b741d14a4ddf5
Sam Stewart <git@alt.ci>
2023-12-06 17:12:34 +0000

    Additional exports for `mod.ts`

--- mod.ts
+++ mod.ts
@@ -1,5 +1,14 @@
 export { extract, parse } from "./src/index.ts";
 
+export { extractMail } from "./src/extractor.ts";
+
+export {
+  parseBody,
+  parseContentType,
+  parseHeaders,
+  parseHeaderValue,
+} from "./src/parser.ts";
+
 export type {
   LetterparserAttachment,
   LetterparserContentType,
